5db0c2cb14f26ae905c1ee7dcf8a2923b6d973a5
prochor666 <prochor666@gmail.com>
2021-06-19 17:50:31 +0200

    Only shell install process

--- install.cmd
+++ install.cmd
@@ -1,9 +1,48 @@
 @cls
 @WHERE /Q python
+
 @IF %ERRORLEVEL% NEQ 0 (
-@ECHO Python 3 is required
-@EXIT /B 0
+    @ECHO Python 3 is required
+    @EXIT /B 0
+)
+
+@WHERE /Q pip
+
+@IF %ERRORLEVEL% NEQ 0 (
+    @ECHO PIP is required
+    @EXIT /B 0
 ) ELSE (
-pip install pymongo
-@python install.py %*
+    pip install pymongo
+    pip install Flask
+    pip install pyopenssl
+    pip install pyIsEmail
+    pip install psutil
+    pip install websocket
+    pip install websocket-client
+    pip install -U python-digitalocean
+    pip install pyarubacloud
+
+    @IF exist storage\recipes (
+        echo storage\recipes exists
+    ) ELSE (
+        mkdir storage\recipes && echo storage\recipes created
+    )
+
+    @IF exist storage\templates (
+        echo storage\templates exists
+    ) ELSE (
+        mkdir storage\templates && echo storage\templates created
+    )
+
+    @IF exist json\app.json (
+        echo json\app.json exists
+    ) ELSE (
+        copy json\sample.app.json json\app.json && echo json\app.json created
+    )
+
+    @IF exist json\smtp.json (
+        echo storage\recipes exists
+    ) ELSE (
+        copy json\sample.smtp.json json\smtp.json && echo json\smtp.json created
+    )
 )
