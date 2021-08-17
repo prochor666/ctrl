938ec14a681b8c6279317cc356bd6092329f28cb
prochor666 <prochor666@gmail.com>
2021-08-18 00:18:59 +0200

    Monitoring resources cpu/RAM/network/webstorage

--- core/ctrl/monit.py
+++ core/ctrl/monit.py
@@ -7,7 +7,7 @@ def survey(id):
     result = {
         'status': False,
         'message': f"Server not found",
-        'shell': []
+        'data': {}
     }
 
     monitor = compose_monitor_tasks(id)
@@ -15,14 +15,18 @@ def survey(id):
     if type(monitor) is not dict:
         return result
 
-    result = remote.init_client(monitor['server'], monitor['tasks'])
-    result['shell'] = ''.join(result['shell'])
+    raw_result = ''.join(
+        remote.init_client(monitor['server'], monitor['tasks'])['shell'])
 
-    parts = parse_monitor_result(result['shell'])
+    parts = parse_monitor_result(raw_result)
 
-    result['shell'] = stat.cpu(parts['stats1'], parts['stats2'])
-    result['shell'] = stat.memory(parts['memory'])
+    result['data']['cpu'] = stat.cpu(parts['cpu'])
+    result['data']['load'] = stat.cpu_load(parts['stats1'], parts['stats2'])
+    result['data']['memory'] = stat.memory(parts['memory'])
+    result['data']['storage'] = stat.storage(parts['storage'])
+    result['data']['network'] = stat.network(parts['network'], parts['network_stats'])
     result['status'] = True
+    result['message'] = f"Server {monitor['server']['name']} stat data"
 
     return result
 
@@ -49,15 +53,18 @@ def compose_monitor_tasks(id):
         'echo "<control-monitor-stat-sample2>"',
         'echo "$(cat /proc/stat)"',
         'echo "</control-monitor-stat-sample2>"',
-        'echo "<control-monitor-mem>"',
+        'echo "<control-monitor-memory>"',
         'echo "$(cat /proc/meminfo)"',
-        'echo "</control-monitor-mem>"',
+        'echo "</control-monitor-memory>"',
         'echo "<control-monitor-storage>"',
-        'echo "$(df -h /var/www)"',
+        'echo "$(df -P /var/www)"',
         'echo "</control-monitor-storage>"',
-        'echo "<control-monitor-net>"',
+        'echo "<control-monitor-network>"',
         'echo "$(cat /proc/net/dev)"',
-        'echo "</control-monitor-net>"',
+        'echo "</control-monitor-network>"',
+        'echo "<control-monitor-network-stats>"',
+        'echo "TO-DO: create stat service https://gist.github.com/prochor666/f5dc193a1fe01b9bef2c1e2cdbfa4e70"',
+        'echo "</control-monitor-network-stats>"',
     ]
 
     return {
@@ -68,9 +75,12 @@ def compose_monitor_tasks(id):
 
 def parse_monitor_result(raw):
 
+    print(raw)
+
     cpu = utils.tag_parse('control-monitor-cpu', raw)
-    memory = utils.tag_parse('control-monitor-mem', raw)
-    net = utils.tag_parse('control-monitor-net', raw)
+    memory = utils.tag_parse('control-monitor-memory', raw)
+    network = utils.tag_parse('control-monitor-network', raw)
+    network_stats = utils.tag_parse('control-monitor-network-stats', raw)
     storage = utils.tag_parse('control-monitor-storage', raw)
     stats1 = utils.tag_parse('control-monitor-stat-sample1', raw)
     stats2 = utils.tag_parse('control-monitor-stat-sample2', raw)
@@ -78,7 +88,8 @@ def parse_monitor_result(raw):
     return {
         'cpu': cpu,
         'memory': memory,
-        'net': net,
+        'network': network,
+        'network_stats': network_stats,
         'storage': storage,
         'stats1': stats1,
         'stats2': stats2,
--- core/ctrl/stat.py
+++ core/ctrl/stat.py
@@ -1,8 +1,36 @@
 import re
 from core import utils
 
+
+def network(sample, stats):
+
+    return [sample, stats]
+
+
+def storage(sample):
+    lines = sample.strip().split("\n")
+    stat = {}
+
+    if len(lines)>1:
+        line = lines[1].split()
+        total = int(line[1].strip())*1024
+        free = int(line[3].strip())*1024
+        used = int(line[2].strip())*1024
+
+        res = {
+            'total': total,
+            'free': free,
+            'used': used,
+            'total_human': utils.byte_size(total),
+            'free_human': utils.byte_size(free),
+            'used_human': utils.byte_size(used),
+        }
+
+    return res
+
+
 def memory(sample):
-    lines = sample.split("\n")
+    lines = sample.strip().split("\n")
     stat = {}
 
     for line in lines:
@@ -11,10 +39,9 @@ def memory(sample):
         if reg is not None:
             info = line.split(':')
             key = str(info[0].strip())
-            value = info[1].strip()
+            value = str(info[1].strip())
             value = int(value.split(' ')[0])
             stat[key] = value*1024
-
 
     total = stat['MemTotal']
     free = stat['MemFree'] + stat['Buffers'] + stat['Cached']
@@ -32,7 +59,29 @@ def memory(sample):
     return res
 
 
-def cpu(sample1, sample2):
+def cpu(sample):
+    lines = sample.strip().split("\n")
+    cores = {}
+    index = -1
+
+    for line in lines:
+
+        reg = re.search(f"^processor|^model name|^core id|^cpu MHz|^cache size", line)
+        if reg is not None:
+            info = line.split(':')
+            key = str(info[0].strip())
+            value = str(info[1].strip())
+            value = str(value.split(' ')[0])
+
+            if key == 'processor':
+                index = value
+                cores[index] = {}
+
+            cores[index][key] = value
+
+    return cores
+
+def cpu_load(sample1, sample2):
     stat1 = core_info(sample1)
     stat2 = core_info(sample2)
     cpu = cpu_percentage(stat1, stat2)
@@ -41,13 +90,13 @@ def cpu(sample1, sample2):
 
 def core_info(sample):
 
-    lines = sample.split("\n")
+    lines = sample.strip().split("\n")
     data = []
     for line in lines:
         reg = re.findall(f"^cpu[0-9]", line)
 
         if len(reg)>0:
-            info = line.split(' ')
+            info = line.split()
             data.append({
                 'user': info[1],
                 'nice': info[2],
