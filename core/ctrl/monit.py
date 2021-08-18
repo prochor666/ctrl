6f4d969652694cb4869a7751838bae3a370f822a
prochor666 <prochor666@gmail.com>
2021-08-18 13:01:30 +0200

    Monitoring data parsing

--- core/ctrl/monit.py
+++ core/ctrl/monit.py
@@ -18,13 +18,15 @@ def survey(id):
     raw_result = ''.join(
         remote.init_client(monitor['server'], monitor['tasks'])['shell'])
 
+    #print(raw_result)
+
     parts = parse_monitor_result(raw_result)
 
     result['data']['cpu'] = stat.cpu(parts['cpu'])
     result['data']['load'] = stat.cpu_load(parts['stats1'], parts['stats2'])
     result['data']['memory'] = stat.memory(parts['memory'])
     result['data']['storage'] = stat.storage(parts['storage'])
-    result['data']['network'] = stat.network(parts['network'], parts['network_stats'])
+    result['data']['network'] = stat.network(parts['network1'], parts['network2'], parts['network_stats'])
     result['status'] = True
     result['message'] = f"Server {monitor['server']['name']} stat data"
 
@@ -42,29 +44,7 @@ def compose_monitor_tasks(id):
         return False
 
     tasks = [
-        'echo "SSH server hostname: $(hostname)"',
-        'echo "<control-monitor-cpu>"',
-        'echo "$(cat /proc/cpuinfo)"',
-        'echo "</control-monitor-cpu>"',
-        'echo "<control-monitor-stat-sample1>"',
-        'echo "$(cat /proc/stat)"',
-        'echo "</control-monitor-stat-sample1>"',
-        'sleep 2',
-        'echo "<control-monitor-stat-sample2>"',
-        'echo "$(cat /proc/stat)"',
-        'echo "</control-monitor-stat-sample2>"',
-        'echo "<control-monitor-memory>"',
-        'echo "$(cat /proc/meminfo)"',
-        'echo "</control-monitor-memory>"',
-        'echo "<control-monitor-storage>"',
-        'echo "$(df -P /var/www)"',
-        'echo "</control-monitor-storage>"',
-        'echo "<control-monitor-network>"',
-        'echo "$(cat /proc/net/dev)"',
-        'echo "</control-monitor-network>"',
-        'echo "<control-monitor-network-stats>"',
-        'echo "TO-DO: create stat service https://gist.github.com/prochor666/f5dc193a1fe01b9bef2c1e2cdbfa4e70"',
-        'echo "</control-monitor-network-stats>"',
+        'cat /opt/ctrl/stats/control-monitor.data',
     ]
 
     return {
@@ -79,7 +59,8 @@ def parse_monitor_result(raw):
 
     cpu = utils.tag_parse('control-monitor-cpu', raw)
     memory = utils.tag_parse('control-monitor-memory', raw)
-    network = utils.tag_parse('control-monitor-network', raw)
+    network1 = utils.tag_parse('control-monitor-network1', raw)
+    network2 = utils.tag_parse('control-monitor-network2', raw)
     network_stats = utils.tag_parse('control-monitor-network-stats', raw)
     storage = utils.tag_parse('control-monitor-storage', raw)
     stats1 = utils.tag_parse('control-monitor-stat-sample1', raw)
@@ -88,7 +69,8 @@ def parse_monitor_result(raw):
     return {
         'cpu': cpu,
         'memory': memory,
-        'network': network,
+        'network1': network1,
+        'network2': network2,
         'network_stats': network_stats,
         'storage': storage,
         'stats1': stats1,
--- core/ctrl/stat.py
+++ core/ctrl/stat.py
@@ -1,15 +1,107 @@
 import re
+import math
+from slugify import slugify
 from core import utils
 
 
-def network(sample, stats):
+def network(sample1, sample2, stats):
+    devices_data = get_alive_devices(stats)
+    print(devices_data)
+    devices = get_net_streams(devices_data, sample1, sample2)
 
-    return [sample, stats]
+    return devices
+
+
+def get_net_streams(devices_data, sample1, sample2):
+    lines1 = sample1.strip().split("\n")
+    lines1.pop(0)
+    lines2 = sample2.strip().split("\n")
+    lines2.pop(0)
+    streams = []
+
+    print(sample1, sample2)
+
+    for line in lines1:
+        i = lines1.index(line)
+        info1 = line.strip().split(':')
+        info2 = lines2[i].strip().split(':')
+
+        device_key = info1[0].strip()
+
+        if device_key in devices_data:
+            stream = parse_net_streams(info1[1], info2[1])
+
+            streams.append({
+                'device': device_key,
+                'rx': devices_data[device_key]['RX'],
+                'tx': devices_data[device_key]['TX'],
+                'downstream': stream['downstream'],
+                'upstream': stream['upstream'],
+                'human_rx': utils.byte_size(int(devices_data[device_key]['RX'])),
+                'human_tx': utils.byte_size(int(devices_data[device_key]['TX'])),
+                'human_downstream': stream['human_downstream'],
+                'human_upstream': stream['human_upstream']
+            })
+
+    return streams
+
+
+def parse_net_streams(line1, line2):
+    data1 = line1.strip().split()
+    data2 = line2.strip().split()
+
+    stream = {
+        'downstream': (abs(int(data2[0]) - int(data1[0]))*8)/1000000,
+        'upstream': (abs(int(data2[0]) - int(data1[0]))*8)/1000000,
+        'human_downstream': utils.byte_size(abs(int(data2[0]) - int(data1[0]))),
+        'human_upstream': utils.byte_size(abs(int(data2[1]) - int(data1[1])))
+    }
+
+    return stream
+
+
+def get_alive_devices(stats):
+    lines = stats.strip().split("\n")
+    devices = {}
+    index = 'none'
+
+    for line in lines:
+        reg = re.search(f"^Device|^RX|^TX", line)
+
+        if reg is not None:
+            info = line.split(':')
+            key = str(info[0].strip())
+            value = str(info[1].strip())
+
+            if key == 'Device':
+                value = str(value.split(' ')[0])
+                index = value
+            else:
+                value = int(value.split(' ')[0])
+
+            if index != 'none':
+
+                if index not in devices:
+                    devices[index] = {}
+
+                devices[index][key] = value
+
+    return devices
+
 
 
 def storage(sample):
     lines = sample.strip().split("\n")
     stat = {}
+
+    res = {
+        'total': 0,
+        'free': 0,
+        'used': 0,
+        'total_human': utils.byte_size(0),
+        'free_human': utils.byte_size(0),
+        'used_human': utils.byte_size(0),
+    }
 
     if len(lines)>1:
         line = lines[1].split()
@@ -43,8 +135,12 @@ def memory(sample):
             value = int(value.split(' ')[0])
             stat[key] = value*1024
 
-    total = stat['MemTotal']
-    free = stat['MemFree'] + stat['Buffers'] + stat['Cached']
+    total = stat['MemTotal'] if 'MemTotal' in stat else 0
+    _free = stat['MemFree'] if 'MemFree' in stat else 0
+    _buffers = stat['Buffers'] if 'Buffers' in stat else 0
+    _cached = stat['Cached'] if 'Cached' in stat else 0
+
+    free = _free + _buffers + _cached
     used = total - free
 
     res = {
@@ -69,9 +165,13 @@ def cpu(sample):
         reg = re.search(f"^processor|^model name|^core id|^cpu MHz|^cache size", line)
         if reg is not None:
             info = line.split(':')
-            key = str(info[0].strip())
+            key = slugify(str(info[0].strip()), separator='_')
             value = str(info[1].strip())
             value = str(value.split(' ')[0])
+
+            if key == 'cpu_mhz':
+                key = 'cpu_frequency'
+                value = utils.decimal_size(math.ceil(float(value)*1000000))
 
             if key == 'processor':
                 index = value
@@ -80,6 +180,7 @@ def cpu(sample):
             cores[index][key] = value
 
     return cores
+
 
 def cpu_load(sample1, sample2):
     stat1 = core_info(sample1)
